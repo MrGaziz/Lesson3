9fb56dcdfad02316c67743e382c0cd6b7256ed66
Dasmor <morhuhn123321@gmail.com>
2019-10-30 00:30:37 +0600

    Lesson 3

---
+++ src/services/implementation/ObjectHandlerIntImpl.java
@@ -0,0 +1,12 @@
+package services.implementation;
+
+import services.ObjectHandler;
+
+import java.util.List;
+
+public class ObjectHandlerIntImpl implements ObjectHandler {
+    public void handleObject(List<Object> collection){
+        //Find max Value
+        //If its not Int handle error
+    }
+}
