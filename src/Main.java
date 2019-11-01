7385fd79f0ba761632502da6aab7110e27746645
Aldiyar Sakhan <aldiyar.sakhan@gmail.com>
2019-11-01 16:54:38 +0600

    implementation ObjectHandlerIntImpl

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import services.implementation.ObjectHandlerIntImpl;
 import services.implementation.RandomGeneratorImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         RandomGeneratorImpl testGenerator = new RandomGeneratorImpl();
         System.out.println("Test collection:\n"+testGenerator.generateCollection(10));
 
-
-    }
+     }
 }
--- src/services/implementation/ObjectHandlerIntImpl.java
+++ src/services/implementation/ObjectHandlerIntImpl.java
@@ -7,6 +7,18 @@ import java.util.List;
 public class ObjectHandlerIntImpl implements ObjectHandler {
     public void handleObject(List<Object> collection){
         //Find max Value
-        //If its not Int handle error
+        //If its not Int handle error (через трай кэч)
+        int max = 0;
+        try {
+            max = (int) collection.get(0);
+            for (int i = 1; i<collection.size(); i++){
+                if((int)collection.get(i) > max){
+                    max = (int) collection.get(i);
+                }
+            }
+        }catch (Exception e){
+            System.out.println("That is not an integer, please try again." );
+        }
+        System.out.println(max);
     }
 }
